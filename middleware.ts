bced8f451feb7277ee0a98cde11930356c72641f
Vincent Zhang <vzhang@box.com>
2025-09-27 18:19:10 -0700

    fixed response check

--- middleware.ts
+++ middleware.ts
@@ -8,17 +8,14 @@ export function middleware(request: NextRequest) {
 
   const { pathname } = request.nextUrl;
 
-  // Allow access to initial landing page and authentication API routes
-  if (pathname === '/' || 
-      pathname.startsWith('/api/auth/box-auth')) {
-    return NextResponse.next();
-  }
-
   // For all other paths, check for authentication
   const accessCookie = request.cookies.get('auth_code');
-  if (!accessCookie) {
-    // If no auth cookie, redirect to the root
-    return NextResponse.redirect(new URL('/', request.url));
+  if (pathname !== '/' ||
+    !pathname.startsWith('/api/auth/box-auth')) {
+    if (!accessCookie) {
+      // If no auth cookie, redirect to the root
+      return NextResponse.redirect(new URL('/', request.url));
+    }
   }
 
   return NextResponse.next();
