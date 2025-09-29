dad49c8d043ab54f6393772d1def6b4ee78e6dc5
Vincent Zhang <vzhang@box.com>
2025-09-29 02:02:33 -0700

    change to all server side components final4

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -8,24 +8,28 @@ import { cookies } from 'next/headers';
 export default async function MainPageContent() {
   try {
 
-    const cookieStore = await cookies()
+    async function getUserCookies(auth_code: string, redirect_to_box_url: string) {
+      "use server"
+      const cookieStore = await cookies()
 
 
-    const userId = await cookieStore.get('user_id');
-    const userName = await cookieStore.get('user_name');
-    if (!userId || !userName) {
-      throw new Error('User ID or user name not found');
-    }
+      const userId = await cookieStore.get('user_id');
+      const userName = await cookieStore.get('user_name');
+      if (!userId || !userName) {
+        throw new Error('User ID or user name not found');
+      }
 
-    // Authenticate user
-    const authenticatedUser = await checkAdmin(userId.value, userName.value);
+      // Authenticate user
+      const authenticatedUser = await checkAdmin(userId.value, userName.value);
 
-    // Set default section based on user role
-    const initialSection = authenticatedUser.isAdmin ? 'admin' : 'recertification';
+      // Set default section based on user role
+      const initialSection = authenticatedUser.isAdmin ? 'admin' : 'recertification';
 
-    return (
+      return (
         <MainPageClient user={authenticatedUser} initialSection={initialSection} />
-    )
+      )
+      await getUserCookies(auth_code, redirect_to_box_url);
+    }
 
   } catch (error: any) {
     console.error('Error in MainPageContent:', error);
